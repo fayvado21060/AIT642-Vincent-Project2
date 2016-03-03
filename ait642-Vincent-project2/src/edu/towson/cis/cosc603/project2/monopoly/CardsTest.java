065a4ab534553d1ec2e1c4048bb83657abe6de1c
Faye <fayvado@yahoo.com>
2016-03-02 21:05:59 -0500

    Commit after changing TYPE_CHANCE and changing code back

--- ait642-Vincent-project2/src/edu/towson/cis/cosc603/project2/monopoly/CardsTest.java
+++ ait642-Vincent-project2/src/edu/towson/cis/cosc603/project2/monopoly/CardsTest.java
@@ -13,14 +13,14 @@ public class CardsTest extends TestCase {
         gameMaster.setNumberOfPlayers(1);
         gameMaster.reset();
         gameMaster.setGUI(new MockGUI());
-        ccCard = new MoneyCard("Get 50 dollars", 50, Card.TYPE_CC);
+        ccCard = new MoneyCard("Get 50 dollars", 50, Card.TYPE_CHANCE);
         chanceCard = new MoneyCard("Lose 50 dollars", -50, Card.TYPE_CHANCE);
         gameMaster.getGameBoard().addCard(ccCard);
     }
     
     public void testCardType() {
-        gameMaster.drawCCCard();
-        assertEquals(Card.TYPE_CC, ccCard.getCardType());
+        gameMaster.drawChanceCard();
+        assertEquals(Card.TYPE_CHANCE, ccCard.getCardType());
         gameMaster.drawChanceCard();
         assertEquals(Card.TYPE_CHANCE, chanceCard.getCardType());
     }
