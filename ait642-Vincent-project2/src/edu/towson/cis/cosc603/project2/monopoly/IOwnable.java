67564054eb95e2ea61a7b8869a4fb90ee582b0a6
Faye <fayvado@yahoo.com>
2016-03-04 20:07:51 -0500

    Task #8 Create an interface IOwnable

---
+++ ait642-Vincent-project2/src/edu/towson/cis/cosc603/project2/monopoly/IOwnable.java
@@ -0,0 +1,48 @@
+package edu.towson.cis.cosc603.project2.monopoly;
+
+public interface IOwnable {
+
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
+	public abstract String getName();
+
+	/**
+	 * Gets the theOwner.
+	 *
+	 * @return the theOwner
+	 */
+	public abstract Player getTheOwner();
+
+	/**
+	 * Gets the price.
+	 *
+	 * @return the price
+	 */
+	public abstract int getPrice();
+
+	/**
+	 * Sets the theOwner.
+	 *
+	 * @param theOwner the new theOwner
+	 */
+	
+	public abstract void setTheOwner(Player owner);
+
+	/**
+	 * Checks if is available.
+	 *
+	 * @return true, if is available
+	 */
+	public abstract boolean isAvailable();
+
+	/**
+	 * Sets the available.
+	 *
+	 * @param available the new available
+	 */
+	public abstract void setAvailable(boolean available);
+
+}
